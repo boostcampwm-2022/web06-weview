05b43998f8ab53825980b3a38f1fab06fbed00d4
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-12 14:57:50 +0900

    fix(FE): 윈도우와 팝업 사이 통신이 되지 않는 버그 수정 (#49)

--- client/src/hooks/useOAuthPopup.tsx
+++ client/src/hooks/useOAuthPopup.tsx
@@ -4,7 +4,6 @@ interface OAuthPopup {
   popup: Window | null;
   clearPopup: () => void;
   handleOpenOAuthPopup: () => void;
-  getAuthCode: () => string | null;
 }
 
 const useOAuthPopup = (): OAuthPopup => {
@@ -34,25 +33,7 @@ const useOAuthPopup = (): OAuthPopup => {
     setPopup(null);
   }, [popup]);
 
-  // 팝업 URL 에서 Github Authorizaion Code 를 가져옵니다.
-  const getAuthCode = useCallback((): string | null => {
-    if (popup === null) {
-      return null;
-    }
-
-    const popupUrl = popup.location.href;
-    if (popupUrl === null) {
-      console.log("url 없음");
-      return null;
-    }
-
-    const searchParams = new URL(popupUrl).searchParams;
-    const code = searchParams.get("code"); // "code" 쿼리스트링 혹은 null 반환
-
-    return code;
-  }, [popup]);
-
-  return { popup, clearPopup, handleOpenOAuthPopup, getAuthCode };
+  return { popup, clearPopup, handleOpenOAuthPopup };
 };
 
 export default useOAuthPopup;
