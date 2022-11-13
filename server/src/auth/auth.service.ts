b0d6cacb859c8ffbf13085870cc4a25977368a46
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-13 17:19:05 +0900

    feat(BE): UserService에 JWT 의존성 주입 (#56)

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../domain/user/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async inquery(email: string) {
+    return {
+      email: null,
+      nickname: null,
+      profileUrl: null,
+    };
+  }
+
+  async join(email: string, nickname: string, avatarUrl: string) {}
+
+  login(email: string) {
+    return {
+      accessToken: null,
+      refreshToken: null,
+    };
+  }
+}
