f4b0586a7590d79e95aa918b4e38d54adba0c58e
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-14 16:25:52 +0900

    feat(FE): 인피니티 스크롤 바닥을 감지하여 불러오는 기능을 커스텀 훅으로 구현 (#60)

---
+++ client/src/hooks/usePostInfiniteScroll.tsx
@@ -0,0 +1,67 @@
+import {
+  FetchNextPageOptions,
+  InfiniteData,
+  InfiniteQueryObserverResult,
+  QueryFunctionContext,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { fetchPost } from "@/apis/post";
+import { PostScroll } from "@/types/post";
+import { useCallback } from "react";
+
+interface PostInfiniteScrollOptions {
+  size: number;
+}
+
+interface PostInfiniteScrollResults {
+  data: InfiniteData<PostScroll> | undefined;
+  hasNextPage: boolean | undefined;
+  isFetching: boolean;
+  fetchNextPage: (
+    options?: FetchNextPageOptions | undefined
+  ) => Promise<InfiniteQueryObserverResult<PostScroll, unknown>>;
+  onIntersect: (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver
+  ) => void;
+}
+
+/**
+ * react-query 를 이용한 포스트 정보에 대한 인피니티 스크롤 커스텀 훅 입니다.
+ */
+const usePostInfiniteScroll = ({
+  size,
+}: PostInfiniteScrollOptions): PostInfiniteScrollResults => {
+  const { data, hasNextPage, isFetching, fetchNextPage } = useInfiniteQuery(
+    ["posts"],
+    async ({ pageParam = 0 }: QueryFunctionContext) =>
+      await fetchPost(pageParam, size),
+    {
+      getNextPageParam: (lastPost) =>
+        lastPost.isLast ? undefined : lastPost.lastId,
+    }
+  );
+
+  /**
+   * Intersection Observer 를 위한 콜백 함수
+   * 다음 페이지를 로딩해야 하는 상황을 감지했을 때 fetchPost 요청을 보내서 가져옵니다.
+   */
+  const onIntersect = useCallback(
+    (
+      entry: IntersectionObserverEntry,
+      observer: IntersectionObserver
+    ): void => {
+      observer.unobserve(entry.target);
+      if (hasNextPage === true && !isFetching) {
+        fetchNextPage()
+          .then(() => {})
+          .catch((e) => {});
+      }
+    },
+    [hasNextPage, isFetching, fetchNextPage]
+  );
+
+  return { data, hasNextPage, isFetching, fetchNextPage, onIntersect };
+};
+
+export default usePostInfiniteScroll;
