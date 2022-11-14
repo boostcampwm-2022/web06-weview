f5db43576a65a07a1e208d98b644b5957a9126a7
Jiwon Kim <lost731@naver.com>
2022-11-14 23:48:40 +0900

    design(FE): 글쓰기 모달 코드 에디터 레이아웃 (#59)

---
+++ client/src/components/WriteModal/CodeEditor/CodeEditor.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CodeEditor = (): JSX.Element => {
+  return (
+    <textarea className="code-editor" autoComplete="false" spellCheck="false" />
+  );
+};
+
+export default CodeEditor;
