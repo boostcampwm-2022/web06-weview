0af9a4eb2d56c75b6eed3d4895902a85cce69de0
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-14 14:02:30 +0900

    refactor(BE): Github의 정보 받아오는 로직을 Service로 이동 (#58)

--- server/src/domain/auth/auth.controller.ts
+++ server/src/domain/auth/auth.controller.ts
@@ -1,4 +1,3 @@
-import { HttpService } from '@nestjs/axios';
 import {
   Controller,
   HttpCode,
@@ -10,53 +9,23 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
-import { lastValueFrom } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly configService: ConfigService,
-    private readonly httpService: HttpService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Get('auth/github')
   async authorizeWithGithub(
     @Res({ passthrough: true }) res: Response,
     @Query('code') code: string,
   ) {
-    // TODO Service 계층으로 분리하기
-    const { data } = await lastValueFrom(
-      this.httpService.post('https://github.com/login/oauth/access_token', {
-        client_id: this.configService.get<string>('AUTH_GITHUB_CLIENT_ID'),
-        client_secret: this.configService.get<string>('AUTH_GITHUB_SECRET_KEY'),
-        code: code,
-      }),
-    );
+    const userInfo = await this.authService.getUserInfoUsingGithub(code);
 
-    const githubToken = data.match(/(?<=access_token=).*?(?=&)/)[0];
-    const { data: githubInfo } = await lastValueFrom(
-      this.httpService.get('https://api.github.com/user', {
-        headers: {
-          Authorization: `Bearer ${githubToken}`,
-        },
-      }),
-    );
+    const { email, nickname, avatarUrl } = userInfo;
 
-    const { data: emails } = await lastValueFrom(
-      this.httpService.get('https://api.github.com/user/emails', {
-        headers: {
-          Authorization: `Bearer ${githubToken}`,
-        },
-      }),
-    );
-
-    const { email } = emails.filter((v) => v.primary)[0];
-    const { avatar_url: avatarUrl, login: nickname } = githubInfo;
     let user = await this.authService.findByEmail(email);
     if (user == null) {
       user = await this.authService.join(email, nickname, avatarUrl);
--- server/src/domain/auth/auth.service.ts
+++ server/src/domain/auth/auth.service.ts
@@ -3,6 +3,9 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../user/user.entity';
+import { lastValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AuthService {
@@ -10,6 +13,8 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
     private jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
   ) {}
 
   async join(email: string, nickname: string, avatarUrl: string) {
@@ -61,5 +66,40 @@ export class AuthService {
       nickname: user.nickname,
       profileUrl: user.profileUrl,
     };
+  }
+
+  async getUserInfoUsingGithub(code: string) {
+    const githubToken = await this.getGithubToken(code);
+
+    const { data: githubInfo } = await lastValueFrom(
+      this.httpService.get('https://api.github.com/user', {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+      }),
+    );
+    const { data: emails } = await lastValueFrom(
+      this.httpService.get('https://api.github.com/user/emails', {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+      }),
+    );
+    const { email } = emails.filter((v) => v.primary)[0];
+    const { avatar_url: avatarUrl, login: nickname } = githubInfo;
+    return { email, nickname, avatarUrl };
+  }
+
+  private async getGithubToken(code: string) {
+    const { data } = await lastValueFrom(
+      this.httpService.post('https://github.com/login/oauth/access_token', {
+        client_id: this.configService.get<string>('AUTH_GITHUB_CLIENT_ID'),
+        client_secret: this.configService.get<string>('AUTH_GITHUB_SECRET_KEY'),
+        code: code,
+      }),
+    );
+
+    const githubToken = data.match(/(?<=access_token=).*?(?=&)/)[0];
+    return githubToken;
   }
 }
