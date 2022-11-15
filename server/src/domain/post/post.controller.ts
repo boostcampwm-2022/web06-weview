a578708e34ccbfc4b18637a4bdf6d1291d71a2ad
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-16 02:51:18 +0900

    feat(BE): 게시물 목록 조회 API 1차 구현 (#73)
     프론트와 연결을 해보기 위해 우선 사용자리스트, 좋아요 개수, 카테고리로만 필터링을 진행합니다.

--- server/src/domain/post/post.controller.ts
+++ server/src/domain/post/post.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PostService } from './post.service';
+import { InqueryUsingFilterDto } from './dto/controller-response.dto';
+import { LoadPostListResponseDto } from './dto/service-response.dto';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @Get()
+  async inqueryUsingFilter(
+    @Query('lastId') lastId: number,
+    @Query('category') category: string,
+    @Query('tags') tags?: string[],
+    @Query('authors') authors?: string[],
+    @Query('writtenAnswer') writtenAnswer?: number,
+    @Query('scores') scores?: number,
+  ): Promise<LoadPostListResponseDto> {
+    if (tags === undefined) {
+      tags = [];
+    }
+    if (authors === undefined) {
+      authors = [];
+    }
+    const result = await this.postService.loadPostList(
+      Number(lastId), //TODO 다른 방법 찾기
+      3,
+      authors,
+      category as Category,
+      scores,
+    );
+    return result;
+  }
 }
