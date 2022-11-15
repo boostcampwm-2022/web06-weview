2a35e4f505644ff06d29afaa3ef7c67c18db7d01
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2022-11-16 02:29:37 +0900

    feat(BE): CORS 관련 설정 추가, 토큰 내 유저가 유효하지 않을 때 거절하도록 구현 (#6)

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
+    origin: ['http://localhost:8000'],
     credentials: true,
   });
   await app.listen(8000);
