6948284b5aec746dad7a18669b7a5a992d7f9c9d
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-17 23:33:56 +0900

    feat(FE): 이미지 로딩 체크 (#107)

--- client/src/components/PostBar/Post/PostImageSlider/PostImageSlider.tsx
+++ client/src/components/PostBar/Post/PostImageSlider/PostImageSlider.tsx
@@ -1,12 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useLayoutEffect, useState } from "react";
 import { PostContext } from "@/components/PostBar/Post/Post";
+import { isEmpty } from "@/utils/typeCheck";
 
 const PostImageSlider = (): JSX.Element => {
   const { images } = useContext(PostContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    if (isEmpty(images)) {
+      return;
+    }
+    setIsLoading(false);
+  }, [images]);
 
   return (
     <div className="post__image-slider">
-      <img className="post__image-slider--image" src={images[0].src} />
+      {isLoading ? (
+        <div className="login-callback__spinner"></div>
+      ) : (
+        <img className="post__image-slider--image" src={images[0].src} />
+      )}
     </div>
   );
 };
