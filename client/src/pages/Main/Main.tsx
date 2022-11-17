5f62c114e40fd1981e278c7f12c3b1566eab02d7
Jiwon Kim <lost731@naver.com>
2022-11-17 16:55:43 +0900

    design(FE): 글쓰기 모달 디자인 및 애니메이션 수정 (#95)

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import MainNav from "@/components/MainNav/MainNav";
 import PostBar from "@/components/PostBar/PostBar";
+import useModalStore from "@/store/useModalStore";
 
 const Main = (): JSX.Element => {
+  const { isWritingModalOpened } = useModalStore((state) => ({
+    isWritingModalOpened: state.isWritingModalOpened,
+  }));
+
   return (
-    <div className="main">
+    <div className={isWritingModalOpened ? "hidden-main" : "main"}>
       <MainNav />
       <PostBar />
     </div>
