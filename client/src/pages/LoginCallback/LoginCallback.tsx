4237d1a4fb8dd31ddb84fcf223906870d20789c7
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-17 23:08:18 +0900

    refactor(FE): FE-BE API 동기화에 따른 변경사항 리팩토링 (#107)

--- client/src/pages/LoginCallback/LoginCallback.tsx
+++ client/src/pages/LoginCallback/LoginCallback.tsx
@@ -1,3 +1,4 @@
+import { LOCAL_URL } from "@/constants/env";
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import "./LoginCallback.scss";
@@ -8,7 +9,7 @@ const LoginCallback = (): JSX.Element => {
   useEffect(() => {
     const code = searchParams.get("code");
     const targetWindow = window.opener;
-    const targetOrigin = "http://localhost:5173/";
+    const targetOrigin = LOCAL_URL;
 
     /**
      * 팝업을 생성한 Window 에 OAuth Authorization Code 를 전달한다.
