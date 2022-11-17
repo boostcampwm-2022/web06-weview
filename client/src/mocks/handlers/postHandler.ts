19621e9095d7ee88a504e602c3bab79375f731f6
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-17 23:07:36 +0900

    test(FE): FE-BE API 동기화에 따른 MOCK API 수정 (#107)

--- client/src/mocks/handlers/postHandler.ts
+++ client/src/mocks/handlers/postHandler.ts
@@ -1,21 +1,28 @@
 import { PostInfo } from "@/types/post";
 import { rest } from "msw";
+import { API_SERVER_URL } from "@/constants/env";
 
 // Backend API Server URL
-const baseUrl = import.meta.env.VITE_API_SERVER_URL;
+const baseUrl = API_SERVER_URL;
 
 const posts = Array.from(Array(1024).keys()).map(
   (id): PostInfo => ({
     id: `${id}`,
     title: `title${id}`,
     content: `post_contents_${id}`,
-    imageUrls: [
-      "http://placeimg.com/640/640/animals",
-      "http://placeimg.com/640/640/animals",
+    images: [
+      {
+        src: "http://placeimg.com/640/640/animals",
+        name: "image1",
+      },
+      {
+        src: "http://placeimg.com/640/640/animals",
+        name: "image1",
+      },
     ],
-    user: {
+    author: {
       id: `${id + 10000}`,
-      username: `sampleUser_${id + 10000}`,
+      nickname: `sampleUser_${id + 10000}`,
       profileUrl: "http://placeimg.com/640/640/animals",
       email: `name_${id + 10000}@gmail.com`,
     },
@@ -23,27 +30,28 @@ const posts = Array.from(Array(1024).keys()).map(
     reviews: [],
     updatedAt: "2022-11-16 12:26:56.124939",
     code: `sourcecode: ~~~~~~~~~~~~~~~~~~`,
+    language: `javascript`,
   })
 );
 
 export const postHandlers = [
-  rest.get(`${baseUrl}api/posts`, (req, res, ctx) => {
-    const lastId = Number(req.url.searchParams.get("lastId"));
-    const size = Number(req.url.searchParams.get("size"));
-    const isLast = posts.length <= lastId + size;
+  rest.get(`${baseUrl}/posts`, (req, res, ctx) => {
+    const lastId = Number(req.url.searchParams.get("lastId")) + 1;
+    const SIZE = 3;
+    const isLast = posts.length <= lastId + SIZE;
 
     return res(
       ctx.status(200),
       ctx.delay(1000),
       ctx.json({
-        posts: posts.slice(lastId, lastId + size),
-        lastId: lastId + size,
+        posts: posts.slice(lastId, lastId + SIZE),
+        lastId: lastId + SIZE,
         isLast,
       })
     );
   }),
 
-  rest.post(`${baseUrl}api/posts`, (req, res, ctx) => {
+  rest.post(`${baseUrl}/posts`, (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({ message: "글 작성에 성공했습니다." })
