d3cf7d17850c8ba8105751cbf1b2c0de4047fce2
Jiwon Kim <lost731@naver.com>
2022-11-17 16:05:42 +0900

    refactor(FE): 해시태그 찾는 정규표현식 상수로 분리 (#85)

--- client/src/utils/regExpression.ts
+++ client/src/utils/regExpression.ts
@@ -1,7 +1,9 @@
 export const LINE_COUNT_REGEX = /\r\n|\r|\n/;
 
+export const FIND_ALL_HASH_TAG_REGEX = /#[^\s#]+/g;
+
 export const getHashTags = (content: string): string[] | undefined =>
-  content.match(/#[^\s#]+/g)?.map((tag) => tag.slice(1));
+  content.match(FIND_ALL_HASH_TAG_REGEX)?.map((tag) => tag.slice(1));
 
 export const formatTag = (tag: string): string =>
   tag.replace(/_/g, "-").toLowerCase();
