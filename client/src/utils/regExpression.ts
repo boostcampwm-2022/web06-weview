d173275560647a1930cdec41c8f2543ec058f2bd
Jiwon Kim <lost731@naver.com>
2022-11-19 15:57:25 +0900

    feat(FE): 글 작성 시 코드 xss 방지 처리 (#126)

--- client/src/utils/regExpression.ts
+++ client/src/utils/regExpression.ts
@@ -2,8 +2,14 @@ export const LINE_COUNT_REGEX = /\r\n|\r|\n/;
 
 export const FIND_ALL_HASH_TAG_REGEX = /#[^\s#]+/g;
 
+export const OPEN_TAG_REGEX = /</g;
+export const CLOSE_TAG_REGEX = />/g;
+
 export const getHashTags = (content: string): string[] | undefined =>
   content.match(FIND_ALL_HASH_TAG_REGEX)?.map((tag) => tag.slice(1));
 
 export const formatTag = (tag: string): string =>
   tag.replace(/_/g, "-").toLowerCase();
+
+export const preventXSS = (html: string): string =>
+  html.replace(OPEN_TAG_REGEX, "&lt;").replace(CLOSE_TAG_REGEX, "&gt;");
