e46ae39f81ab947e708b44b47d724d7744b0f80d
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2022-11-21 17:03:19 +0900

    refactor(BE): 유저 레포지토리 계층 분리 (#133)

---
+++ server/src/domain/user/user.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+}
