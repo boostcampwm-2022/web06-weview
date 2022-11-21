dcdcb9971d3c0f2b4ad03db686b0244f8c29fb06
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-21 22:38:57 +0900

    feat(FE): Search Modal 구현을 위한 유틸리티 구현 (#137)

---
+++ client/src/constants/search.ts
@@ -0,0 +1,7 @@
+export const SEPARATOR: { [key: string]: string } = {
+  "#": "tag",
+  "@": "category",
+  $: "author",
+  "+": "scores",
+  "^": "writtenAnswer",
+};
--- client/src/utils/pressedKeyCheck.ts
+++ client/src/utils/pressedKeyCheck.ts
@@ -1,2 +1,4 @@
 export const isSubmitKey = (key: string): boolean =>
   key === "Enter" || key === " ";
+
+export const isSpaceKey = (key: string): boolean => key === " ";
