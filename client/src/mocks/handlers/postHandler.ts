90d65cf5e2acfb50ad9ba759c7677c1809c1d648
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-22 21:27:38 +0900

    test(FE): 검색 필터 MOCK API 추가 및 적용 (#137)

--- client/src/mocks/handlers/postHandler.ts
+++ client/src/mocks/handlers/postHandler.ts
@@ -1,6 +1,7 @@
-import { PostInfo } from "@/types/post";
+import { PostInfo, Review } from "@/types/post";
 import { rest } from "msw";
 import { API_SERVER_URL } from "@/constants/env";
+import { parsePostQueryString, review } from "@/mocks/utils/postUtils";
 
 // Backend API Server URL
 const baseUrl = API_SERVER_URL;
@@ -27,25 +28,52 @@ const posts = Array.from(Array(1024).keys()).map(
       email: `name_${id + 10000}@gmail.com`,
     },
     tags: [`tag1`, `tag2`],
-    reviews: [],
+    reviews: Array.from(Array(id % 3).keys()).map(review),
     updatedAt: "2022-11-16 12:26:56.124939",
     code: `sourcecode: ~~~~~~~~~~~~~~~~~~`,
     language: `javascript`,
+    category: id % 2 === 0 ? "리뷰요청" : "질문",
+    likes: id % 10,
   })
 );
 
 export const postHandlers = [
   rest.get(`${baseUrl}/posts`, (req, res, ctx) => {
     const lastId = Number(req.url.searchParams.get("lastId")) + 1;
+    const { tags, authors, category, reviews, likes, detail } =
+      parsePostQueryString(req.url);
     const SIZE = 3;
     const isLast = posts.length <= lastId + SIZE;
+
+    const filteredData = posts
+      .filter(
+        (post) =>
+          tags.length === 0 ||
+          post.tags.some((postTag) => tags.includes(postTag))
+      )
+      .filter(
+        (post) => authors.length === 0 || authors.includes(post.author.nickname)
+      )
+      .filter((post) => category === "" || category === post.category)
+      .filter((post) => reviews <= post.reviews.length)
+      .filter((post) => likes <= (post.likes ?? 0))
+      .filter(
+        (post) =>
+          post.title.search(detail) > -1 || post.content.search(detail) > -1
+      )
+      .slice(lastId, lastId + SIZE);
+
+    console.log(
+      { tags, authors, category, reviews, likes, detail },
+      filteredData.length
+    );
 
     return res(
       ctx.status(200),
       ctx.delay(1000),
       ctx.json({
-        posts: posts.slice(lastId, lastId + SIZE),
-        lastId: lastId + SIZE,
+        posts: filteredData,
+        lastId: lastId + SIZE - 1,
         isLast,
       })
     );
