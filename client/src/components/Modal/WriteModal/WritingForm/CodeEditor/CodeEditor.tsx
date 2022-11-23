a4897fa1f582297ea0805cc665b62fc7562c0ec9
Jiwon Kim <lost731@naver.com>
2022-11-23 15:47:07 +0900

    feat(FE): 코드 에디터 스크롤 기능 추가 (#128)

--- client/src/components/Modal/WriteModal/WritingForm/CodeEditor/CodeEditor.tsx
+++ client/src/components/Modal/WriteModal/WritingForm/CodeEditor/CodeEditor.tsx
@@ -1,16 +1,24 @@
-import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import useWritingStore from "@/store/useWritingStore";
 import hljs from "highlight.js";
-import useLineNumbers from "@/hooks/useLineNumbers";
 
 const CodeEditor = (): JSX.Element => {
   const [highlightedHTML, setHighlightedHTML] = useState("");
-  const { lineCount, setLines } = useLineNumbers();
   const { code, setCode, language } = useWritingStore((state) => ({
     code: state.code,
     setCode: state.setCode,
     language: state.language,
   }));
+
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const preRef = useRef<HTMLPreElement>(null);
+
   useEffect(() => {
     setHighlightedHTML(
       hljs
@@ -31,16 +39,25 @@ const CodeEditor = (): JSX.Element => {
     [code]
   );
 
+  const handleScrollChange = useCallback((): void => {
+    if (textRef.current !== null && preRef.current !== null) {
+      preRef.current.style.height = textRef.current.style.height;
+      preRef.current.scrollTop = textRef.current.scrollTop;
+    }
+  }, [textRef, preRef]);
+
   return (
     <div className="code-editor">
       <textarea
+        ref={textRef}
+        onScroll={handleScrollChange}
         value={code}
         onChange={changeCode}
         className="code-editor__textarea"
         autoComplete="false"
         spellCheck="false"
       />
-      <pre className="code-editor__present">
+      <pre ref={preRef} className="code-editor__present">
         <code
           dangerouslySetInnerHTML={createMarkUpCode(highlightedHTML)}
         ></code>
