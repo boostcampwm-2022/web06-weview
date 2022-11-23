ba9588d884e6cb7da60a2f3c6fcbe01b8aca8009
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-23 23:57:07 +0900

    feat(FE): 리뷰 모달에 리뷰 무한 스크롤 적용 (#146)

--- client/src/components/Modal/ReviewModal/ReviewModal.tsx
+++ client/src/components/Modal/ReviewModal/ReviewModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CodeEditor from "@/components/CodeEditor/CodeEditor";
+import ReviewContainer from "@/components/Modal/ReviewModal/ReviewContainer/ReviewContainer";
 
 interface ReviewModalProps {
   postId: string;
@@ -10,9 +11,7 @@ const ReviewModal = ({ postId, code }: ReviewModalProps): JSX.Element => {
   return (
     <div className="review-modal">
       <CodeEditor />
-      <div className="review-modal__reviews">
-        postId: {postId} code: {code}
-      </div>
+      <ReviewContainer postId={postId} />
     </div>
   );
 };
