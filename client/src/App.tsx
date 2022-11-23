3fe43cffc79e2e80bf1ee5d9f425a0a5ac4ddf5a
Jiwon Kim <lost731@naver.com>
2022-11-23 12:49:30 +0900

    refactor(FE): Modal Wrapper 디렉토리 Modal 디렉토리 내부로 이동 (#146)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { isEmpty } from "@/utils/typeCheck";
 import ReactRouter from "@/ReactRouter";
-import CommonModalWrapper from "@/components/ModalWrapper/CommonModalWrapper";
-import OtherModalWrapper from "@/components/ModalWrapper/OtherModalWrapper";
+import CommonModalWrapper from "@/components/Modal/ModalWrapper/CommonModalWrapper";
+import OtherModalWrapper from "@/components/Modal/ModalWrapper/OtherModalWrapper";
 
 function App(): JSX.Element {
   const queryClientRef = useRef<QueryClient>();
