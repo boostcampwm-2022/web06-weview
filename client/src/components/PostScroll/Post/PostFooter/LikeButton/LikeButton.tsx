9fb2668921bdbbe9a5342fa4aa96a1151aed6a78
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-28 04:22:13 +0900

    design(FE): SvgIconButton 컴포넌트 적용 (#93)

--- client/src/components/PostScroll/Post/PostFooter/LikeButton/LikeButton.tsx
+++ client/src/components/PostScroll/Post/PostFooter/LikeButton/LikeButton.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import SvgIconButton from "@/components/CommonButtons/SvgIconButton/SvgIconButton";
+
+const primaryColor = "#84cb8d";
+const textColor = "#222222";
 
 const LikeButton = (): JSX.Element => {
-  return <ThumbUpAltIcon className="post__footer__left-block--btn" />;
+  const [liked, setLiked] = useState(false);
+
+  const toggleLiked: MouseEventHandler = () => {
+    console.log("좋아요");
+    setLiked((liked) => !liked);
+  };
+
+  const handleRequestLogin: MouseEventHandler = () => {
+    console.log("로그인이 필요합니다.");
+  };
+
+  if (liked === undefined) {
+    return (
+      <SvgIconButton
+        Icon={ThumbUpAltIcon}
+        detail="좋아요"
+        onClick={handleRequestLogin}
+        className="post__footer__left-block--btn"
+      />
+    );
+  }
+
+  return (
+    <SvgIconButton
+      Icon={ThumbUpAltIcon}
+      detail="좋아요"
+      onClick={toggleLiked}
+      className="post__footer__left-block--btn"
+      color={`${liked ? primaryColor : textColor}`}
+    />
+  );
 };
 
 export default LikeButton;
--- client/src/components/PostScroll/Post/PostFooter/ReportButton/ReportButton.tsx
+++ client/src/components/PostScroll/Post/PostFooter/ReportButton/ReportButton.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import SvgIconButton from "@/components/CommonButtons/SvgIconButton/SvgIconButton";
+
+const errorColor = "#f45452";
+const textColor = "#222222";
 
 const ReportButton = (): JSX.Element => {
-  return <ThumbDownIcon className="post__footer__left-block--btn" />;
+  const [reported, setReported] = useState(false);
+
+  const handleReport: MouseEventHandler = () => {
+    console.log("신고하기");
+    setReported((reported) => !reported);
+  };
+
+  return (
+    <SvgIconButton
+      Icon={ThumbDownIcon}
+      detail="신고하기"
+      onClick={handleReport}
+      className="post__footer__left-block--btn"
+      color={`${reported ? errorColor : textColor}`}
+    />
+  );
 };
 
 export default ReportButton;
