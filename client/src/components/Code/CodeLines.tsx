ce014ae1c4fbb4ce182df4d7a046a4c7564467fb
Jiwon Kim <lost731@naver.com>
2022-11-29 16:32:52 +0900

    feat(FE): 코드 이미지로 변환 적용 (#128)

--- client/src/components/Code/CodeLines.tsx
+++ client/src/components/Code/CodeLines.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, RefObject } from "react";
 import { getLineCount } from "@/utils/code";
+import domtoimage from "dom-to-image";
+import useWritingStore from "@/store/useWritingStore";
+import { ONE_SNAP_SHOT_LINE_COUNT } from "@/constants/code";
+import { IMAGE_OPTIONS } from "@/constants/options";
 
 interface CodeLinesProps {
   code: string;
@@ -8,10 +12,23 @@ interface CodeLinesProps {
 
 const CodeLines = ({ code, lineRef }: CodeLinesProps): JSX.Element => {
   const [lineCount, setLineCount] = useState(0);
+  const [setImages] = useWritingStore((state) => [state.setImages]);
 
   useEffect(() => {
     setLineCount(getLineCount(code));
   }, [code]);
+
+  useEffect(() => {
+    if (lineCount === 0 || lineCount % ONE_SNAP_SHOT_LINE_COUNT !== 0) return;
+
+    const $code = document.querySelector(
+      `.chunked-${lineCount / ONE_SNAP_SHOT_LINE_COUNT}`
+    );
+    domtoimage
+      .toJpeg($code as HTMLElement, IMAGE_OPTIONS)
+      .then((dataUrl: string) => setImages(dataUrl))
+      .catch((error: any) => console.error(error));
+  }, [lineCount]);
 
   return (
     <div className="code__lines" ref={lineRef}>
