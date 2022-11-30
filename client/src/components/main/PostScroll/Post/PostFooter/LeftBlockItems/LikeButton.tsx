a36443ea659fefe261513683b0d02c1adfcea50d
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-30 17:58:11 +0900

    style(FE): FE 스타일 정리- 디렉토리 구조, 스타일 파일 구조, 공통 스타일 설정 (#220)

---
+++ client/src/components/main/PostScroll/Post/PostFooter/LeftBlockItems/LikeButton.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import SvgIconButton from "@/components/commons/SvgIconButton/SvgIconButton";
+import usePostLike from "@/hooks/usePostLike";
+import { PostContext } from "@/components/main/PostScroll/Post/Post";
+
+const primaryColor = "#84cb8d";
+const textColor = "#222222";
+
+const LikeButton = (): JSX.Element => {
+  const { id, isLiked } = useContext(PostContext);
+
+  const { isLikedState, toggleLiked } = usePostLike({
+    postId: id,
+    isLiked,
+  });
+
+  return (
+    <SvgIconButton
+      Icon={ThumbUpAltIcon}
+      detail="좋아요"
+      onClick={toggleLiked}
+      className="post__footer__left-block--btn"
+      color={`${isLikedState ? primaryColor : textColor}`}
+    />
+  );
+};
+
+export default LikeButton;
