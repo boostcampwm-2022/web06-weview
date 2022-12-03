4c9f0c3e892e1775423dd3e754f0b839be3b1615
Jiwon Kim <lost731@naver.com>
2022-12-04 01:22:18 +0900

    feat(FE): 인기 태그 스켈레톤 ui 추가 (#274)

---
+++ client/src/components/main/TagRanking/TagRankingSkeletonItem/TagRankingSkeletonItem.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./TagRankingSkeletonItem.scss";
+
+interface TagRankingSkeletonItemProps {
+  rank: number;
+}
+
+const TagRankingSkeletonItem = ({
+  rank,
+}: TagRankingSkeletonItemProps): JSX.Element => {
+  return (
+    <li className="tag-rank-skeleton-item">
+      <span className="tag-rank-skeleton-item__rank">{rank}.</span>
+
+      <div className="tag-rank-skeleton-item__content" />
+    </li>
+  );
+};
+
+export default TagRankingSkeletonItem;
