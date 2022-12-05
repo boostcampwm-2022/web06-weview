fa5f3d60961cac0cc971c0b44f203887d7efe011
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-12-05 22:54:23 +0900

    feat(FE): 최근 검색 기록 불러오는 컴포넌트를 Suspense를 이용하여 구현 (#280)

--- client/src/components/main/MainNav/NavContent/SearchContent/SearchContentBody/SearchContentHistory.tsx
+++ client/src/components/main/MainNav/NavContent/SearchContent/SearchContentBody/SearchContentHistory.tsx
@@ -1,27 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
 import React, { Suspense } from "react";
 
-import { QUERY_KEYS } from "@/react-query/queryKeys";
-import { fetchSearchHistory } from "@/apis/search";
 import SearchHistoryView from "@/components/main/MainNav/NavContent/SearchContent/SearchHistoryView/SearchHistoryView";
+import LoadingSpinner from "@/components/commons/LoadingSpinner/LoadingSpinner";
 
 import "./SearchContentBody.scss";
 
 const SearchContentHistory = (): JSX.Element => {
-  const { data } = useQuery([QUERY_KEYS.HISTORY], fetchSearchHistory, {
-    suspense: true,
-  });
-
   return (
     <div className="search-content__body">
       <div className="title">최근 검색어</div>
-      <Suspense fallback={<div>로딩중..</div>}>
-        {data?.map((history) => (
-          <SearchHistoryView
-            key={`${history.updatedAt}-history`}
-            searchHistory={history}
+      <Suspense
+        fallback={
+          <LoadingSpinner
+            className={"search-content__body--loading"}
+            small={true}
           />
-        ))}
+        }
+      >
+        <SearchHistoryView />
       </Suspense>
     </div>
   );
--- client/src/components/main/MainNav/NavContent/SearchContent/SearchHistoryView/SearchHistoryView.tsx
+++ client/src/components/main/MainNav/NavContent/SearchContent/SearchHistoryView/SearchHistoryView.tsx
@@ -1,15 +1,36 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { SearchHistory } from "@/types/search";
+import { QUERY_KEYS } from "@/react-query/queryKeys";
+import { fetchSearchHistory } from "@/apis/search";
 
-interface SearchHistoryViewProps {
-  searchHistory: SearchHistory;
+interface SearchHistoryItemProps {
+  history: SearchHistory;
 }
 
-const SearchHistoryView = ({
-  searchHistory,
-}: SearchHistoryViewProps): JSX.Element => {
-  return <div>히스토리</div>;
+const SearchHistoryItem = ({
+  history,
+}: SearchHistoryItemProps): JSX.Element => {
+  return <div className="search-content__body__histories--item">아이템</div>;
+};
+
+const SearchHistoryView = (): JSX.Element => {
+  const { data } = useQuery<SearchHistory[] | undefined>(
+    [QUERY_KEYS.HISTORY],
+    fetchSearchHistory,
+    {
+      suspense: true,
+    }
+  );
+
+  return (
+    <div className="search-content__body__histories">
+      {data?.map((history) => (
+        <SearchHistoryItem key={history.updatedAt} history={history} />
+      ))}
+    </div>
+  );
 };
 
 export default SearchHistoryView;
