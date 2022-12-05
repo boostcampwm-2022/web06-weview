b4fd759328d5cc21271a9e47c4a13f1e4125c034
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-12-06 00:51:19 +0900

    fix(FE): FE-BE API 명세 일치 오류 수정 (#303)

--- client/src/apis/search.ts
+++ client/src/apis/search.ts
@@ -5,6 +5,6 @@ import { SearchHistory } from "@/types/search";
  * 사용자의 최근 검색어를 요청하는 함수
  */
 export const fetchSearchHistory = async (): Promise<SearchHistory[]> => {
-  const { data } = await axiosInstance.get(`/search/history`);
+  const { data } = await axiosInstance.get(`/search/histories`);
   return data;
 };
