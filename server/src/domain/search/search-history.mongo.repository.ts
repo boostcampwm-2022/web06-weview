02ee93e47a82dac1b29a048435dc46053b4f91c1
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2022-12-05 17:35:51 +0900

    feat(BE): 검색 기록에 대한 MongoDB 엔티티, 레포지토리 구현 (#280)

---
+++ server/src/domain/search/search-history.mongo.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, MongoRepository } from 'typeorm';
+import { SearchHistory } from './search-history.mongo';
+
+@Injectable()
+export class SearchHistoryMongoRepository extends MongoRepository<SearchHistory> {
+  constructor(@InjectDataSource('mongo') private datasource: DataSource) {
+    super(SearchHistory, datasource.mongoManager);
+  }
+
+  async findAllByUserId(userId: number) {
+    return await this.find({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  async addSearchHistory(
+    userId,
+    tags,
+    author,
+    reviewCount,
+    likeCount,
+    details,
+  ) {
+    let searchHistory = await this.findOne({
+      where: {
+        userId,
+        tags,
+        author,
+        reviewCount,
+        likeCount,
+        details,
+      },
+    });
+
+    if (!searchHistory) {
+      searchHistory = new SearchHistory(
+        userId,
+        tags,
+        author,
+        reviewCount,
+        likeCount,
+        details,
+      );
+    }
+
+    this.save(searchHistory);
+  }
+}
---
+++ server/src/domain/search/search-history.mongo.ts
@@ -0,0 +1,43 @@
+import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+import { BaseTimeEntity } from '../base-time.entity';
+
+@Entity()
+export class SearchHistory extends BaseTimeEntity {
+  @ObjectIdColumn()
+  _id: ObjectID;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  tags: string[];
+
+  @Column()
+  author: string;
+
+  @Column()
+  reviewCount: number;
+
+  @Column()
+  likeCount: number;
+
+  @Column()
+  details: string;
+
+  constructor(
+    userId: number,
+    tags: string[],
+    author: string,
+    reviewCount: number,
+    likeCount: number,
+    details: string,
+  ) {
+    super();
+    this.userId = userId;
+    this.tags = tags;
+    this.author = author;
+    this.reviewCount = reviewCount;
+    this.likeCount = likeCount;
+    this.details = details;
+  }
+}
