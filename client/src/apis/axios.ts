b62b86c4ee48bc4e11b6e86387e69b415d5a375e
Jiwon Kim <lost731@naver.com>
2022-12-05 22:32:57 +0900

    fix(FE): refresh 관련 interceptor 훅으로 만들어 상태와 동기화 (#295)

--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -2,11 +2,6 @@ import axios, { AxiosRequestConfig } from "axios";
 
 import { API_SERVER_URL } from "@/constants/env";
 
-import {
-  refreshErrorHandler,
-  refreshInterceptor,
-} from "./interceptors/refresh";
-
 const axiosConfig: AxiosRequestConfig = {
   baseURL: API_SERVER_URL,
   timeout: 10000,
@@ -14,7 +9,5 @@ const axiosConfig: AxiosRequestConfig = {
 };
 
 const axiosInstance = axios.create(axiosConfig);
-
-axiosInstance.interceptors.request.use(refreshInterceptor, refreshErrorHandler);
 
 export default axiosInstance;
