c2920f4c1ffe5f64c1349f803249d4dc238ed7b9
Jiwon Kim <lost731@naver.com>
2022-12-06 15:51:37 +0900

    feat(FE): 공유 버튼 누를 시에 클립보드에 복사되는 기능 추가 (#313)

--- client/src/components/main/PostScroll/Post/PostFooter/LeftBlockItems/ShareButton.tsx
+++ client/src/components/main/PostScroll/Post/PostFooter/LeftBlockItems/ShareButton.tsx
@@ -1,12 +1,22 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEvent, useContext } from "react";
 import ShareIcon from "@mui/icons-material/Share";
 
 import SvgIconButton from "@/components/commons/SvgIconButton/SvgIconButton";
+import { LOCAL_URL } from "@/constants/env";
+import { PostContext } from "@/components/main/PostScroll/Post/Post";
 
 import "./LeftBlockItems.scss";
 
 const ShareButton = (): JSX.Element => {
-  const handleShare: MouseEventHandler = () => {};
+  const { id: postId } = useContext(PostContext);
+  const handleShare = (e: MouseEvent): void => {
+    navigator.clipboard
+      .writeText(`${LOCAL_URL}/post/${postId}`)
+      .then(() => {
+        alert("클립보드에 해당 포스트의 링크가 복사되었습니다.");
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <SvgIconButton
