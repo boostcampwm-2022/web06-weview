8c55b34ced6f4450437efdc8596e1cb31c73fff7
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-12-07 06:03:33 +0900

    feat(FE): Post Body Content 더보기 기능 추가 (#222)

--- client/src/components/main/PostScroll/Post/PostBody/PostBody.tsx
+++ client/src/components/main/PostScroll/Post/PostBody/PostBody.tsx
@@ -1,9 +1,45 @@
-import React, { useContext } from "react";
+import React, { MouseEventHandler, useContext, useState } from "react";
 
 import { PostContext } from "@/components/main/PostScroll/Post/Post";
 import TimeStamp from "@/components/commons/TimeStamp/TimeStamp";
+import {
+  MAXIMUM_CONTENT_ENTER,
+  MAXIMUM_CONTENT_LENGTH,
+} from "@/constants/code";
 
 import "./PostBody.scss";
+
+interface PostContentProps {
+  content: string;
+}
+
+const PostContent = ({ content }: PostContentProps): JSX.Element => {
+  const contentSlice = content.slice(0, MAXIMUM_CONTENT_LENGTH);
+  const contentSliceEnterCount = contentSlice.split("\n").length - 1;
+
+  const [isOpened, setIsOpened] = useState(
+    contentSlice.length < MAXIMUM_CONTENT_LENGTH && // 더보기로 나누지 않을 만큼 내용이 충분히 짧음
+      contentSliceEnterCount < MAXIMUM_CONTENT_ENTER // 더보기로 나누지 않을 만큼 개행이 적음
+  );
+
+  const handleOpenContent: MouseEventHandler = () => {
+    if (isOpened) {
+      return;
+    }
+    setIsOpened(true);
+  };
+
+  return (
+    <div className="post__body--content">
+      {isOpened ? content : content.slice(0, MAXIMUM_CONTENT_LENGTH)}
+      {!isOpened && (
+        <p className="post__body--content--more" onClick={handleOpenContent}>
+          더보기..
+        </p>
+      )}
+    </div>
+  );
+};
 
 const PostBody = (): JSX.Element => {
   const { title, content, updatedAt } = useContext(PostContext);
@@ -17,7 +53,7 @@ const PostBody = (): JSX.Element => {
           className={"post__body__title--time-stamp"}
         />
       </div>
-      <div className="post__body--content">{content}</div>
+      <PostContent content={content} />
     </div>
   );
 };
