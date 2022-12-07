ec37e484ceb11a53617a88796835dcfd5185189d
Jiwon Kim <lost731@naver.com>
2022-12-08 02:46:46 +0900

    feat(FE): 이미지 클릭 시 코드 + 리뷰 모달이 올라오는 기능 추가 (#354)

--- client/src/components/commons/ProgressiveImage/ProgressiveImage.tsx
+++ client/src/components/commons/ProgressiveImage/ProgressiveImage.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
 
 import useImageIntersect from "@/hooks/useImageIntersect";
+import useCommonModalStore from "@/store/useCommonModalStore";
+import ReviewModal from "@/components/main/Modal/ReviewModal/ReviewModal";
+import { PostContext } from "@/components/main/PostScroll/Post/Post";
 
 interface ProgressiveImageProps {
   className: string;
@@ -20,12 +23,19 @@ const ProgressiveImage = ({
   alt,
 }: ProgressiveImageProps): JSX.Element => {
   const { observeImage } = useImageIntersect();
+  const { id: postId, code, language } = useContext(PostContext);
+  const [openModal] = useCommonModalStore((state) => [state.openModal]);
+
+  const handleOpenReviewModal = useCallback(() => {
+    openModal(<ReviewModal postId={postId} code={code} language={language} />);
+  }, [openModal]);
 
   return (
     <img
       ref={observeImage}
       className={`progressive-image ${className}`}
       src={placeholder}
+      onClick={handleOpenReviewModal}
       data-lazysrc={src}
       width={width}
       height={height}
