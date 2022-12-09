977103dbec3548f81643e7a5a3853ab43d0c5d3c
Jiwon Kim <lost731@naver.com>
2022-12-10 00:10:05 +0900

    feat(FE): 언어 바뀔 때 자동 스냅샷 수정 기능 추가 (#350)

--- client/src/components/main/Modal/WriteModal/WritingForm/LanguageSelector/LanguageSelector.tsx
+++ client/src/components/main/Modal/WriteModal/WritingForm/LanguageSelector/LanguageSelector.tsx
@@ -3,16 +3,35 @@ import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined
 
 import { LANGUAGES } from "@/constants/options";
 import useCodeEditorStore from "@/store/useCodeEditorStore";
+import { ONE_SNAPSHOT_LINE_COUNT } from "@/constants/code";
+import { getLineCount } from "@/utils/code";
+import useCodeEditor from "@/hooks/useCodeEditor";
+import { wait } from "@/utils/async";
 
 import "./LanguageSelector.scss";
 
 const LanguageSelector = (): JSX.Element => {
-  const { selectedLanguage, setLanguage } = useCodeEditorStore((state) => ({
-    selectedLanguage: state.language,
-    setLanguage: state.setLanguage,
-  }));
+  const { selectedLanguage, setLanguage, code, resetImages } =
+    useCodeEditorStore((state) => ({
+      selectedLanguage: state.language,
+      setLanguage: state.setLanguage,
+      images: state.images,
+      code: state.code,
+      resetImages: state.resetImages,
+    }));
+  const { snapShotEachCode } = useCodeEditor();
   const selectLanguage = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
     setLanguage(e.target.value);
+    const requiredSnapshotCount = Math.floor(
+      (getLineCount(code) - 1) / ONE_SNAPSHOT_LINE_COUNT
+    );
+    resetImages();
+    // images 배열이 완벽히 비어지고 highlighting 될 때를 기다려야한다.
+    void (async () => {
+      await wait(1000);
+      for (let idx = 1; idx <= requiredSnapshotCount; idx++)
+        await snapShotEachCode(idx);
+    })();
   }, []);
 
   return (
