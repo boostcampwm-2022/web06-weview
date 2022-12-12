bb5013916fa6ba1539b8abc85b7f6381a7dd98b5
Jiwon Kim <lost731@naver.com>
2022-12-12 12:51:39 +0900

    feat(FE): 로그인 안 된 상태로 새 포스트 클릭 시 로그인 페이지로 유도 (#393)

--- client/src/components/main/MainNav/NavMenus/NewPostMenu/NewPostMenu.tsx
+++ client/src/components/main/MainNav/NavMenus/NewPostMenu/NewPostMenu.tsx
@@ -5,12 +5,20 @@ import NavMenu from "@/components/main/MainNav/NavMenus/NavMenu/NavMenu";
 import useCommonModalStore from "@/store/useCommonModalStore";
 import WriteModal from "@/components/main/Modal/WriteModal/WriteModal";
 import useNav from "@/hooks/useNav";
+import useAuthStore from "@/store/useAuthStore";
+import useAuth from "@/hooks/useAuth";
 
 const NewPostMenu = (): JSX.Element => {
   const { handleWrite } = useNav();
   const [openModal] = useCommonModalStore((state) => [state.openModal]);
-
+  const myInfo = useAuthStore((state) => state.myInfo);
+  const { handleLogin } = useAuth();
   const handleMenuClick: MouseEventHandler = () => {
+    if (myInfo === null) {
+      alert("로그인이 필요합니다.");
+      handleLogin();
+      return;
+    }
     handleWrite(() => openModal(<WriteModal />));
   };
 
